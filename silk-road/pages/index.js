c799b265bda1cc43933d981dca3196b9668b3204
Razvan <daniciucrazvan@yahoo.com>
2022-07-03 16:26:20 +0300

    Initial commit

---
+++ silk-road/pages/index.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+import Header from '../public/Components/Header/Header'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Header/>
+    </div>
+  )
+}
---
+++ silk-road/public/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import styles from './Header.module.css'
+
+export default function Header() {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+}
